20403d0b6522cca7239fbfd74046aaa93ab1a6aa
periBernfeld <perib3782@gmail.com>
2023-04-26 13:08:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+// using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using TodoApi;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<todoApiContext>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+});
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+// app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/items", async (todoApiContext db) =>
+  await db.Items.ToListAsync());
+
+  app.MapPost("/items", async (Item todo,todoApiContext db) =>
+{
+    db.Items.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/items/{todo.Id}", todo);
+});
+
+app.MapPut("/items/{id}", async (int id, Item inputTodo, todoApiContext db) =>
+{
+    var todo = await db.Items.FindAsync(id);
+
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/items/{id}", async (int id, todoApiContext db) =>
+{
+    if (await db.Items.FindAsync(id) is Item todo)
+    {
+        db.Items.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(todo);
+    }
+
+    return Results.NotFound();
+});
+
+app.Run();
